28cbe2e2e07ce7097c850e81681f73c5b1755e73
jesse-chelin <64459814+jesse-chelin@users.noreply.github.com>
2022-09-05 13:00:10 +1000

    No idea

--- app.js
+++ app.js
@@ -305,3 +305,30 @@ const printCards = () => {
 userName.innerHTML = NAME;
 printCards();
 updateDate();
+
+$(document).ready(function() {
+   var accordionTitle = $('.accordion-title');
+   var accordionText = $('.accordion-text');
+   var speed = 300;
+
+   accordionTitle.on('click', function() {
+       var thisAccordion = $(this);
+       var isActive = thisAccordion.hasClass('active');
+
+       if( isActive ) {
+           thisAccordion
+               .removeClass('active')
+               .parent().find('.accordion-text').slideUp(speed);
+       } else {
+           accordionTitle.removeClass('active');
+           accordionText.slideUp(speed);
+           thisAccordion
+               .addClass('active')
+               .parent().find('.accordion-text').slideDown(speed);
+       }
+   });
+
+   if($(window).width() >= 1024){
+    accordionTitle.first().click();
+   }
+});
